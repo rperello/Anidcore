958c718bd63bc81c9cd8f9fa5874b3d290ce2770
Javier Aguilar <javier@anid.es>
2012-05-09 19:00:22 +0200

    simplified Ac::__callStatic

--- system/classes/ac.php
+++ system/classes/ac.php
@@ -32,21 +32,14 @@ class Ac extends Ac_System {
     }
 
     public static function __callStatic($name, $arguments) {
-        if (preg_match("/^on[A-Z]/", $name) || preg_match("/^on$/", $name)) {
-            $event = lcfirst(preg_replace("/^(on)/", "", $name));
-            if (!empty($event))
-                array_unshift($arguments, $event);
-            return call_user_func_array(array(self::observer(), 'on'), $arguments);
-        }elseif (preg_match("/^trigger[A-Z]/", $name) || preg_match("/^trigger$/", $name)) {
-            $event = lcfirst(preg_replace("/^(trigger)/", "", $name));
-            if (!empty($event))
-                array_unshift($arguments, $event);
-            return call_user_func_array(array(self::observer(), 'trigger'), $arguments);
-        }elseif (preg_match("/^off[A-Z]/", $name) || preg_match("/^off$/", $name)) {
-            $event = lcfirst(preg_replace("/^(off)/", "", $name));
-            if (!empty($event))
-                array_unshift($arguments, $event);
-            return call_user_func_array(array(self::observer(), 'off'), $arguments);
+        $fns = array("on", "trigger", "off");
+        foreach($fns as $fn){
+            if (preg_match("/^{$fn}[A-Z]/", $name) || preg_match("/^{$fn}$/", $name)) {
+                $event = lcfirst(preg_replace("/^({$fn})/", "", $name));
+                if (!empty($event))
+                    array_unshift($arguments, $event);
+                return call_user_func_array(array(self::observer(), $fn), $arguments);
+            }
         }
     }
 
